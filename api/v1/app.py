beff87616cda1b36cec7291b0da8f71ccf251a2a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-08-02 02:31:57 +0100

    feat: Handle page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,13 @@
 """
 The application entry point
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
@@ -17,6 +16,12 @@ app.register_blueprint(app_views)
 def close_db(obj):
     """Calls close method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Page not found error"""
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
